7e2a2eda362b644d779cc3c34a731cfc2a4b70e6
AlberLC <alberlc@outlook.com>
2022-01-28 03:48:15 +0100

    Update Place model

--- flanaapis/geolocation/models.py
+++ flanaapis/geolocation/models.py
@@ -10,14 +10,33 @@ from flanautils.models.bases import FlanaBase
 class Place(FlanaBase):
     """Simple class for manage Place data."""
 
-    name: str
-    latitude: float
-    longitude: float
+    name: str = None
+    latitude: float = None
+    longitude: float = None
+    country: str = None
+    country_code: str = None
+    state: str = None
+    state_district: str = None
+    county: str = None
+    city: str = None
+    borough: str = None
+    postcode: str = None
+    neighbourhood: str = None
+    road: str = None
+    number: str = None
+    amenity: str = None
 
-    def __init__(self, name: str, latitude: float, longitude: float):
-        self.name = ', '.join(flanautils.data_structures.ordered_set.OrderedSet(name.split(', ')))
-        self.latitude = float(latitude)
-        self.longitude = float(longitude)
+    def __post_init__(self):
+        if self.name:
+            self.name = ', '.join(flanautils.data_structures.ordered_set.OrderedSet(self.name.split(', ')))
+        if self.latitude:
+            self.latitude = float(self.latitude)
+        if self.longitude:
+            self.longitude = float(self.longitude)
+
+    def __str__(self):
+        address_parts = [address_part for address_part in (self.city, self.state_district, self.state, self.country) if address_part]
+        return ', '.join(address_parts) if address_parts else self.name
 
     def distance_to(self, place: Place) -> float:
         """Calculate the distance between two places."""
