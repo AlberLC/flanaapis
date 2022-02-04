e6b99e28711f93a93c2cc7217d3c22dcd640b1f2
AlberLC <alberlc@outlook.com>
2022-02-04 22:04:25 +0100

    Update Place model

--- flanaapis/geolocation/models.py
+++ flanaapis/geolocation/models.py
@@ -1,42 +1,53 @@
 from __future__ import annotations  # todo0 remove in 3.11
 
-from dataclasses import dataclass
-
 import flanautils
 from flanautils.models.bases import FlanaBase
 
 
-@dataclass(unsafe_hash=True)
 class Place(FlanaBase):
     """Simple class for manage Place data."""
 
-    name: str = None
-    latitude: float = None
-    longitude: float = None
-    country: str = None
-    country_code: str = None
-    state: str = None
-    state_district: str = None
-    county: str = None
-    city: str = None
-    borough: str = None
-    postcode: str = None
-    neighbourhood: str = None
-    road: str = None
-    number: str = None
-    amenity: str = None
-
-    def __post_init__(self):
-        if self.name:
-            self.name = ', '.join(flanautils.data_structures.ordered_set.OrderedSet(self.name.split(', ')))
-        if self.latitude:
-            self.latitude = float(self.latitude)
-        if self.longitude:
-            self.longitude = float(self.longitude)
+    def __init__(
+        self,
+        name: str = None,
+        latitude: float = None,
+        longitude: float = None,
+        country: str = None,
+        country_code: str = None,
+        state: str = None,
+        state_district: str = None,
+        county: str = None,
+        city: str = None,
+        borough: str = None,
+        postcode: str = None,
+        neighbourhood: str = None,
+        road: str = None,
+        number: str = None,
+        amenity: str = None
+    ):
+        self._name = ', '.join(flanautils.data_structures.ordered_set.OrderedSet(name.split(', '))) if name else name
+        self.latitude = float(latitude) if latitude else latitude
+        self.longitude = float(longitude) if longitude else longitude
+        self.country = country
+        self.country_code = country_code
+        self.state = state
+        self.state_district = state_district
+        self.county = county
+        self.city = city
+        self.borough = borough
+        self.postcode = postcode
+        self.neighbourhood = neighbourhood
+        self.road = road
+        self.number = number
+        self.amenity = amenity
 
     def __str__(self):
+        return self.name
+
+    @property
+    def name(self):
         address_parts = [address_part for address_part in (self.city, self.state_district, self.state, self.country) if address_part]
-        return ', '.join(address_parts) if address_parts else self.name
+        return ', '.join(address_parts) if address_parts else self._name
 
     def distance_to(self, place: Place) -> float:
         """Calculate the distance between two places."""
