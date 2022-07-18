4e0b6a1139645986e90bf1fae75ad6c3224f1fc3
AlberLC <alberlc@outlook.com>
2022-07-18 21:53:04 +0200

    Fix google_maps.find_place

--- flanaapis/geolocation/google_maps.py
+++ flanaapis/geolocation/google_maps.py
@@ -3,9 +3,8 @@ import urllib.parse
 from typing import AsyncIterable
 
 import playwright
-from playwright.async_api import async_playwright
-
 from flanaapis.geolocation.models import Place
+from playwright.async_api import async_playwright
 
 
 async def find_place(place_query: str) -> Place | None:
@@ -44,10 +43,11 @@ async def find_place_showing_progress(place_query: str) -> AsyncIterable[str | P
                 page: playwright.async_api.Page = await browser.new_page()
                 yield 'Dirigiéndome a google.es/maps...'
                 await page.goto(f"https://www.google.es/maps/search/{'+'.join(place_query.split())}")
-                button = page.locator("'Acepto'")
-                if await button.count():
+
+                if await (button := page.locator("button:has-text('Acepto')")).count() or await (button := page.locator("button:has-text('Aceptar todo')")).count():
                     yield 'Aceptando consentimiento de privacidad...'
-                    await button.click()
+                    await button.first.click()
+
                 yield 'Rebuscando coordenadas en la página...'
                 while '@' not in page.url:
                     await page.wait_for_event('framenavigated')
