82023138e6f8c5d9bb868e582293f03c42955ed6
AlberLC <alberlc@outlook.com>
2022-08-04 23:02:56 +0200

    Fix YouTube scraper async

--- flanaapis/scraping/youtube.py
+++ flanaapis/scraping/youtube.py
@@ -1,3 +1,4 @@
+import asyncio
 import pathlib
 import re
 import subprocess
@@ -25,18 +26,20 @@ async def get_medias(text: str) -> OrderedSet[Media]:
 
     for youtube_url in youtube_urls:
         try:
-            medias.add(Media(video_bytes(youtube_url), MediaType.VIDEO, Source.YOUTUBE))
+            medias.add(Media(await video_bytes(youtube_url), MediaType.VIDEO, Source.YOUTUBE))
         except ValueError:
             medias.add(Media(type_=MediaType.ERROR, source=Source.YOUTUBE))
 
     return medias
 
 
-def video_bytes(url: str) -> bytes:
+async def video_bytes(url: str) -> bytes:
     yt = pytube.YouTube(url)
 
     video_stream = yt.streams.filter(type='video').order_by('resolution').order_by('filesize').desc().first()
-    audio_stream = yt.streams.filter(type='audio').order_by('bitrate').desc().first()
+    audio_mp3_stream = yt.streams.filter(type='audio', subtype='mp3').order_by('bitrate').desc().first()
+    audio_mp4_stream = yt.streams.filter(type='audio', subtype='mp4').order_by('bitrate').desc().first()
+    audio_stream = audio_mp3_stream if getattr(audio_mp3_stream, 'bitrate', 0) >= getattr(audio_mp4_stream, 'bitrate', 0) else audio_mp4_stream
 
     video_file_name = f'video.{video_stream.subtype}'
     audio_file_name = f'audio.{audio_stream.subtype}'
@@ -44,7 +47,8 @@ def video_bytes(url: str) -> bytes:
     video_stream.download(filename=video_file_name)
     audio_stream.download(filename=audio_file_name)
 
-    subprocess.run(['ffmpeg', '-y', '-i', video_file_name, '-i', audio_file_name, '-c', 'copy', 'output.mp4'], stderr=subprocess.DEVNULL)
+    process = await asyncio.create_subprocess_exec('ffmpeg', '-y', '-i', video_file_name, '-i', audio_file_name, '-c', 'copy', 'output.mp4', stderr=subprocess.DEVNULL)
+    await process.wait()
 
     with open('output.mp4', 'rb') as file:
         video_bytes_ = file.read()
