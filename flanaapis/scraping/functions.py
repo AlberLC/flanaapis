317acd6d9c07278e7aa4683197e0f589507ca75d
AlberLC <alberlc@outlook.com>
2022-11-30 01:12:06 +0100

    Improve legibility

--- flanaapis/scraping/functions.py
+++ flanaapis/scraping/functions.py
@@ -10,17 +10,16 @@ async def filter_audios(medias: Iterable[Media]) -> OrderedSet[Media]:
     for media in medias:
         if not media.content:
             continue
-
         try:
             bytes_ = await flanautils.to_mp3(media.bytes_ or await flanautils.get_request(media.url))
         except ValueError:
-            pass
-        else:
-            new_media = media.deep_copy()
-            new_media.url = None
-            new_media.bytes_ = bytes_
-            new_media.type_ = MediaType.AUDIO
-            new_media.extension = 'mp3'
-            filtered_medias.add(new_media)
+            continue
+
+        new_media = media.deep_copy()
+        new_media.url = None
+        new_media.bytes_ = bytes_
+        new_media.type_ = MediaType.AUDIO
+        new_media.extension = 'mp3'
+        filtered_medias.add(new_media)
 
     return filtered_medias
