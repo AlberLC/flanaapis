a64f342d27f0ab7873449693aa9dc08b9532a787
AlberLC <alberlc@outlook.com>
2026-02-05 08:19:58 +0100

    Fix reddit scraper

--- flanaapis/scraping/reddit.py
+++ flanaapis/scraping/reddit.py
@@ -12,8 +12,16 @@ from flanaapis.exceptions import RedditMediaNotFoundError, ResponseError
 from flanaapis.scraping import constants, functions, instagram, tiktok, twitter
 
 
-def find_ids(text: str) -> OrderedSet[str]:
-    return OrderedSet(re.findall(r'ddit.*ments/(\w+)/', text))
+async def find_ids(text: str) -> OrderedSet[str]:
+    total_ids = OrderedSet()
+
+    for url in flanautils.find_urls(text):
+        total_ids.add_many(re.findall(r'ddit.*ments/(\w+)/', await flanautils.resolve_real_url(url)))
+
+    if ids := re.findall(r'ddit.*ments/(\w+)/', text):
+        total_ids.add_many(ids)
+
+    return total_ids
 
 
 async def get_medias(
@@ -32,8 +40,8 @@ async def get_medias(
     async with aiohttp.ClientSession() as session:
         for url in urls:
             try:
-                data = await flanautils.get_request(url, session=session)
-            except ResponseError:
+                data = await flanautils.get_request(url, headers={'User-Agent': flanautils.USER_AGENT}, session=session)
+            except ResponseError as e:
                 continue
 
             data = data[0]['data']['children'][0]['data']
@@ -91,25 +99,47 @@ async def get_medias_from_data(
                 medias.add(Media(html.unescape(url), MediaType.VIDEO, 'mp4', Source.REDDIT))
 
     # internal hosted videos
-    internal_hosted_video_urls = OrderedSet()
-    internal_hosted_video_urls.add(yt_dlp.utils.traverse_obj(data, ('media', 'reddit_video', 'fallback_url')))
-    internal_hosted_video_urls.add(yt_dlp.utils.traverse_obj(data, ('secure_media', 'reddit_video', 'fallback_url')))
-    internal_hosted_video_urls -= None
-    if internal_hosted_video_urls:
-        for internal_hosted_video_url in internal_hosted_video_urls:
-            video_url = html.unescape(internal_hosted_video_url)
-            audio_url = re.sub(r'_\d+\.mp4', '_audio.mp4', video_url)
-            try:
-                video_bytes = await flanautils.get_request(video_url)
-            except ResponseError:
-                continue
-            try:
-                audio_bytes = await flanautils.get_request(audio_url)
-            except ResponseError:
-                bytes_ = video_bytes
-            else:
-                bytes_ = await flanautils.merge(video_bytes, audio_bytes)
-            medias.add(Media(bytes_, MediaType.VIDEO, source=Source.REDDIT))
+    seen_internal_hosted_video_urls = set()
+
+    for reddit_video_data in (
+            yt_dlp.utils.traverse_obj(data, ('media', 'reddit_video')),
+            yt_dlp.utils.traverse_obj(data, ('secure_media', 'reddit_video'))
+    ):
+        if not reddit_video_data or not (internal_hosted_video_url := reddit_video_data.get('fallback_url')):
+            continue
+
+        video_url = html.unescape(internal_hosted_video_url)
+
+        if video_url in seen_internal_hosted_video_urls:
+            continue
+
+        seen_internal_hosted_video_urls.add(video_url)
+
+        try:
+            video_bytes = await flanautils.get_request(video_url)
+        except ResponseError:
+            continue
+
+        if not reddit_video_data.get('has_audio') or not (dash_url := reddit_video_data.get('dash_url')):
+            medias.add(Media(video_bytes, MediaType.VIDEO, source=Source.REDDIT))
+            continue
+
+        try:
+            playlist_file_bytes = await flanautils.get_request(dash_url)
+        except ResponseError:
+            continue
+
+        matches = re.findall(r'<BaseURL>([^>]+_AUDIO_(\d+)\.mp4)</BaseURL>', str(playlist_file_bytes))
+        best_audio_url = max(matches, key=lambda element: int(element[1]))[0]
+        audio_url = re.sub(r'[^/]+\.mp4', best_audio_url, video_url)
+
+        try:
+            audio_bytes = await flanautils.get_request(audio_url)
+        except ResponseError:
+            continue
+
+        bytes_ = await flanautils.merge(video_bytes, audio_bytes)
+        medias.add(Media(bytes_, MediaType.VIDEO, source=Source.REDDIT))
 
     # external media
     if (
@@ -119,7 +149,7 @@ async def get_medias_from_data(
         and
         not data.get('is_gallery')
         and
-        not internal_hosted_video_urls
+        not seen_internal_hosted_video_urls
         and
         data['url']
     ):
